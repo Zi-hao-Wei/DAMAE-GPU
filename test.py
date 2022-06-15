b30afdc78903f426ab0c95724f7d5780508c56e5
Zi-hao-Wei <weizihao@163.com>
2022-06-15 17:32:10 -0400

    Update

--- test.py
+++ test.py
@@ -8,16 +8,20 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    x = torch.rand(16, 3, 224, 224)
-    y = torch.rand(16, 3, 224, 224)
-    checkpoint = torch.load('/Users/panzixuan/Documents/AI/checkpoints/checkpoint-60.pth', map_location='cpu')
-    model = checkpoint['model']
-    model0 = models_mae.MaskedAutoencoderViT()
-    optimizer = torch.optim.Adam(model0.parameters(), lr=0.05)
-    for i in range(1):
-        criterion = torch.nn.CrossEntropyLoss()
-        optimizer.zero_grad()
-        total_loss, _, _, _, _ = model0(x, y, torch.zeros(1).long())
-        total_loss.backward()
-        optimizer.step()
+    # x = torch.rand(16, 3, 224, 224)
+    # y = torch.rand(16, 3, 224, 224)
+    x1 = range(0,224,16)
+    print(x1)
+    
+
+    # checkpoint = torch.load('/Users/panzixuan/Documents/AI/checkpoints/checkpoint-60.pth', map_location='cpu')
+    # model = checkpoint['model']
+    # model0 = models_mae.MaskedAutoencoderViT()
+    # optimizer = torch.optim.Adam(model0.parameters(), lr=0.05)
+    # for i in range(1):
+    #     criterion = torch.nn.CrossEntropyLoss()
+    #     optimizer.zero_grad()
+    #     total_loss, _, _, _, _ = model0(x, y, torch.zeros(1).long())
+    #     total_loss.backward()
+    #     optimizer.step()
 
